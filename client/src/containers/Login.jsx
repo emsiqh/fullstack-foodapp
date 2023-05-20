4d655a15abc04f119a93a487c37022e2f93ab26a
quang huy <emsiqh>
2023-05-20 17:00:59 +0700

    login page

--- client/src/containers/Login.jsx
+++ client/src/containers/Login.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
 
 import { LoginBg, Logo } from '../assets';
+import { LoginInput } from '../components';
+import { FaEnvelope, FaLock } from '../assets/icons';
+import { buttonClick } from '../animations';
 
 const Login = () => {
+    const [userEmail, setUserEmail] = useState("");
+    const [isSignUp, setIsSignUp] = useState(false);
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
     return (
         <div className='w-screen h-screen relative overflow-hidden flex'>
             {/* Background image */}
@@ -18,14 +27,83 @@ const Login = () => {
 
                 {/* Welcome text */}
                 <p className='text-3xl font-semibold text-headingColor'>Welcome Back</p>
-                <p className='text-xl text-textColor -mt-6'>Sign in with following</p>
+                <p className='text-xl text-textColor -mt-6'>{isSignUp ? 'Sign up' : 'Sign in'} with following</p>
 
                 {/* Input section */}
-                <div className='w-full flex flex-col items-center justify-center gap-6 px-4 md:px-12 py-4'>
-                    <p>a</p>
-                    <p>a</p>
-                    <p>a</p>
+                <div className='w-full flex flex-col items-center justify-center gap-6 px-4 md:px-12 py-1'>
+                    <LoginInput
+                        placeHolder={"Email"}
+                        icon={<FaEnvelope className='text-xl text-textColor' />}
+                        inputState={userEmail}
+                        inputStateFunc={setUserEmail}
+                        type="email"
+                        isSignUp={isSignUp} />
                 </div>
+                <div className='w-full flex flex-col items-center justify-center gap-6 px-4 md:px-12 py-1'>
+                    <LoginInput
+                        placeHolder={"Password"}
+                        icon={<FaLock className='text-xl text-textColor' />}
+                        inputState={password}
+                        inputStateFunc={setPassword}
+                        type="password"
+                        isSignUp={isSignUp} />
+                </div>
+                {
+                    isSignUp && (
+                        <div className='w-full flex flex-col items-center justify-center gap-6 px-4 md:px-12 py-1'>
+                            <LoginInput
+                                placeHolder={"Confirm Password"}
+                                icon={<FaLock className='text-xl text-textColor' />}
+                                inputState={confirmPassword}
+                                inputStateFunc={setConfirmPassword}
+                                type="password"
+                                isSignUp={isSignUp} />
+                        </div>
+                    )
+                }
+
+                {/* Text */}
+                {!isSignUp ? (
+                    <p>
+                        Doesn't have an account:{" "}
+                        <motion.button
+                            {...buttonClick}
+                            className='text-red-700 underline cursor-pointer bg-transparent'
+                            onClick={() => setIsSignUp(true)}
+                        >
+                            Create
+                        </motion.button>
+                    </p>
+                ) : (
+                    <p>
+                        Already have an account:{" "}
+                        <motion.button
+                            {...buttonClick}
+                            className='text-red-700 underline cursor-pointer bg-transparent'
+                            onClick={() => setIsSignUp(false)}
+                        >
+                            Sign-in
+                        </motion.button>
+                    </p>
+                )}
+
+                {/* Button section */}
+                {!isSignUp ? (
+                    <motion.button
+                        {...buttonClick}
+                        className='w-full rounded-md bg-red-400 px-4 py-2 cursor-pointer text-white text-xl capitalize hover:bg-red-500 transition-all duration-150'
+                    >
+                        Sign in
+                    </motion.button>
+                ) : (
+                    <motion.button
+                        {...buttonClick}
+                        className='w-full rounded-md bg-red-400 px-4 py-2 cursor-pointer text-white text-xl capitalize hover:bg-red-500 transition-all duration-150'
+                    >
+                        Sign up
+                    </motion.button>
+                )
+                }
             </div>
 
         </div>
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-
+import { AnimatePresence } from "framer-motion";
 
 import App from "./App";
 import "./index.css";
@@ -11,7 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <AnimatePresence>
+        <App />
+      </AnimatePresence>
     </Router>
   </React.StrictMode>
 );
