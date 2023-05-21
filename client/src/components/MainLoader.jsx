2a42680afa93656209af8a9e317978da6c3a6e8a
quang huy <emsiqh>
2023-05-21 21:35:50 +0700

    main loader

---
+++ client/src/components/MainLoader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/Loader.css';
+
+const MainLoader = () => {
+    return (
+        <div className='flex items-center justify-center container'>
+            <span className="loader"></span>
+        </div>
+    )
+}
+
+export default MainLoader;
