735b38f4c137959e3b4d3a9d4c8d3577c2d8b07d
quang huy <emsiqh>
2023-05-31 21:40:12 +0700

    fix dashboard bug

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -80,12 +80,15 @@ const Header = () => {
                                     className='px-6 py-4 w-44 bg-cardOverlay backdrop-blur-md rounded-md shadow-md absolute top-12 right-0 flex flex-col gap-3'
                                     onMouseLeave={() => setIsMenu(false)}
                                 >
-                                    <Link
-                                        className='hover:text-red-500 text-lg text-textColor'
-                                        to={'/dashboard/home'}
-                                    >
-                                        Dashboard
-                                    </Link>
+                                    {user?.user_id === process.env.REACT_APP_ADMIN_ID && (
+                                        <Link
+                                            className='hover:text-red-500 text-lg text-textColor'
+                                            to={'/dashboard/home'}
+                                        >
+                                            Dashboard
+                                        </Link>
+                                    )}
+
                                     <Link
                                         className='hover:text-red-500 text-lg text-textColor'
                                         to={'/profile'}
